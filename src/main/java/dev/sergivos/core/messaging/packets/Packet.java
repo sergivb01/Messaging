7748c4a89fcd9271e1ae1b519a1e92cc6f0f4254
Sergi Vos <sergivb01@gmail.com>
2021-06-10 23:59:22 +0200

    Packet and PacketSupplier documentation

--- src/main/java/dev/sergivos/core/messaging/packets/Packet.java
+++ src/main/java/dev/sergivos/core/messaging/packets/Packet.java
@@ -3,9 +3,21 @@ package dev.sergivos.core.messaging.packets;
 import io.netty.buffer.ByteBuf;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+
 public interface Packet {
+
+    /**
+     * Deserializes the data from a {@link ByteBuf}
+     *
+     * @param buf The buffer to read from
+     */
     void read(final @NonNull ByteBuf buf);
 
+    /**
+     * Serializes the data into a {@link ByteBuf}
+     *
+     * @param buf The buffer to write data to
+     */
     void write(final @NonNull ByteBuf buf);
 
 }
--- src/main/java/dev/sergivos/core/messaging/packets/PacketSupplier.java
+++ src/main/java/dev/sergivos/core/messaging/packets/PacketSupplier.java
@@ -3,6 +3,33 @@ package dev.sergivos.core.messaging.packets;
 import io.netty.buffer.ByteBuf;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * PacketSupplier is a way to easly create a new instance of a {@link Packet}
+ * without the use of {@code Reflection}. It is used to register a {@link Packet}
+ * to a {@link PacketManager}.
+ *
+ * <h3>Can be used as follows</h3>
+ * <pre>
+ *     packetManager.register(SimplePacket.class, new PacketSupplier<SimplePacket>() {
+ *         \@Override
+ *         public @NotNull SimplePacket create(@NotNull ByteBuf buffer) {
+ *             return new SimplePacket(buffer);
+ *         }
+ *     });
+ * </pre>
+ * <p>
+ * Which simplified will result in
+ * <pre>
+ *     packetManager.register(SimplePacket.class, SimplePacket::new);
+ * </pre>
+ */
 public interface PacketSupplier<T extends Packet> {
+
+    /**
+     * Creates a new instance of a {@link Packet} of {@code T}
+     *
+     * @param buffer The buffer to create the instance from
+     * @return returns a new {@link Packet} {@code T}
+     */
     @NotNull T create(final @NotNull ByteBuf buffer);
 }
